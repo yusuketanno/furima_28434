40cba0c8319f27532076fccf3a4d91753e9d9dc1
yusuketanno <tylf1007@gmail.com>
2020-08-10 07:15:43 +0900

    ユーザー情報編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,23 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :mail, :password)
+  end
+
   protected
 
   def configure_permitted_parameters
