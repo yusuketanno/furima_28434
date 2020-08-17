f7c141a9838e9a3039b9f9e74a53d545edcf6412
yusuketanno <tylf1007@gmail.com>
2020-08-17 15:22:16 +0900

    editアクションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create, :edit]
 end
