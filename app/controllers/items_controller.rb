faa67880db009762cfa7006805ff406e09a1adc5
yusuketanno <tylf1007@gmail.com>
2020-08-22 10:47:09 +0900

    ルーティング修正等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,11 @@ class ItemsController < ApplicationController
     item.update(item_params)
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
 
   def user_params
