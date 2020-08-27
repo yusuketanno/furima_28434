8b6366a6dc9f5f2c3d2655486506e6e1f084571b
yusuketanno <tylf1007@gmail.com>
2020-08-27 15:53:35 +0900

    attr_accessor にtokenを追加

--- app/forms/address_purchase.rb
+++ app/forms/address_purchase.rb
@@ -1,7 +1,7 @@
 class AddressPurchase
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :tell
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :tell, :token
 
   with_options presence: true do
     validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
