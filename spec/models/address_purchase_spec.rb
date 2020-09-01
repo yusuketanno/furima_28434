f3d2dbcffc154a54e60e24ee71a037a271b59e73
yusuketanno <tylf1007@gmail.com>
2020-09-02 06:27:20 +0900

    修正

--- spec/models/address_purchase_spec.rb
+++ spec/models/address_purchase_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AddressPurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   before do
     @address_purchase = FactoryBot.build(:address_purchase)
   end
