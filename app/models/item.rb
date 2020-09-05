4e388a8a4d34a596825b95cd43fc3d18a0e26633
yusuketanno <tylf1007@gmail.com>
2020-09-05 09:29:52 +0900

    商品検索機能実装完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,4 +27,10 @@ class Item < ApplicationRecord
   validates :category, :item_status, :delivery_fee, presence: true
   validates :category_id, :item_status_id, :delivery_fee_id, :delivery_area_id, :delivery_date_id, numericality: { other_than: 1 }
 
+  def self.search(search)
+    if search != ""
+      Item.where(category_id: search)
+      #, item_status_id: search, delivery_fee_id: search, delivery_date_id: search
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   
  root to: "items#index"
+ get 'items/search1'
+ get 'items/search2'
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:edit, :update]
   resources :items do
     resources :purchases, only: [:index, :create]
   end
-  get 'items/search'
+  
 end
