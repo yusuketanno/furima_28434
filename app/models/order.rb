7c2fc1d8dc9707c3a2d2f1a707ddd570efd4f8a5
yusuketanno <tylf1007@gmail.com>
2020-09-08 11:34:49 +0900

    ファイル整理

--- app/models/order.rb
+++
@@ -1,4 +0,0 @@
-class Order < ApplicationRecord
-  validates :price, presence: true
-  belongs_to :user
-end
--- db/migrate/20200824085255_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[6.0]
-  def change
-    create_table :orders do |t|
-      t.integer    :price  ,index: true  ,null: false
-      t.references :user   ,null: false  ,foreign_key: true
-      t.timestamps
-    end
-  end
-end
